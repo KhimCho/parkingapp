5fa764e0457001eb74442746e6674c46a629bb01
KhimCho <95128995+KhimCho@users.noreply.github.com>
2023-12-10 15:12:25 +0700

    add stripe

---
+++ src/routes/stripe.ts
@@ -0,0 +1,5 @@
+import Stripe from "stripe";
+
+export const stripe = new Stripe("sk_test_51OLh0iF0xUpVRl8AiRpflrDr7ttPeFHuX2YbvjbXfUaJLrxEbRtYMidl9lZFkCPtvTjo9NO0n1YSn5ge9NXpZxnX00vP6Ecr7A",{
+    apiVersion: "2023-10-16",
+});
